1eb5bdc0773e7a5f78f733a2c114ca0041fbcc71
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2018-03-08 10:17:55 +0200

    Add skunktoy build target

---
+++ src/main_skunktoy.cpp
@@ -0,0 +1,232 @@
+#ifdef _WIN32
+    #define WIN32_LEAN_AND_MEAN
+    #include <windows.h>
+#endif // _WIN32
+
+#include <GL/gl3w.h>
+#include <GLFW/glfw3.h>
+#include <imgui.h>
+#include <imgui_impl_glfw_gl3.h>
+#include <iostream>
+#include <sstream>
+
+#include "logger.hpp"
+#include "gpuProfiler.hpp"
+#include "quad.hpp"
+#include "shaderProgram.hpp"
+#include "timer.hpp"
+
+#define GUI
+
+using std::cout;
+using std::cerr;
+using std::endl;
+
+namespace {
+    const static char* WINDOW_TITLE = "skunktoy";
+    GLsizei XRES = 1280;
+    GLsizei YRES = 720;
+    float LOGW = 690.f;
+    float LOGH = 210.f;
+    float LOGM = 10.f;
+    GLfloat CURSOR_POS[] = {0.f, 0.f};
+}
+
+void keyCallback(GLFWwindow* window, int32_t key, int32_t scancode, int32_t action,
+                 int32_t mods)
+{
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GL_TRUE);
+#ifdef GUI
+    else
+        ImGui_ImplGlfwGL3_KeyCallback(window, key, scancode, action, mods);
+#endif // GUI
+}
+
+void cursorCallback(GLFWwindow* window, double xpos, double ypos)
+{
+    if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {
+        CURSOR_POS[0] = 2 * xpos / XRES - 1.f;
+        CURSOR_POS[1] = 2 * (YRES - ypos) / YRES - 1.f;
+    }
+}
+
+void mouseButtonCallback(GLFWwindow* window, int button, int action, int mods)
+{
+#ifdef GUI
+    if (ImGui::IsMouseHoveringAnyWindow()) {
+        ImGui_ImplGlfwGL3_MouseButtonCallback(window, button, action, mods);
+        return;
+    }
+#endif //GUI
+
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS) {
+        double xpos, ypos;
+        glfwGetCursorPos(window, &xpos, &ypos);
+        CURSOR_POS[0] = 2 * xpos / XRES - 1.f;
+        CURSOR_POS[1] = 2 * (YRES - ypos) / YRES - 1.f;
+    }
+}
+
+void windowSizeCallback(GLFWwindow* window, int width, int height)
+{
+    (void) window;
+    XRES = width;
+    YRES = height;
+    glViewport(0, 0, XRES, YRES);
+}
+
+static void errorCallback(int error, const char* description)
+{
+    cerr << "GLFW error " << error << ": " << description << endl;
+}
+
+#ifdef _WIN32
+int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PSTR lpCmdLine, INT nCmdShow)
+{
+    (void) hInstance;
+    (void) hPrevInstance;
+    (void) lpCmdLine;
+    (void) nCmdShow;
+#else
+int main()
+{
+#endif // _WIN32
+    // Init GLFW-context
+    glfwSetErrorCallback(errorCallback);
+    if (!glfwInit()) exit(EXIT_FAILURE);
+
+    // Set desired context hints
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    // Create the window
+    GLFWwindow* windowPtr;
+    windowPtr = glfwCreateWindow(XRES, YRES, WINDOW_TITLE, NULL, NULL);
+    if (!windowPtr) {
+        glfwTerminate();
+        cerr << "Error creating GLFW-window!" << endl;
+        exit(EXIT_FAILURE);
+    }
+    glfwMakeContextCurrent(windowPtr);
+
+    // Init GL
+    if (gl3wInit()) {
+        glfwDestroyWindow(windowPtr);
+        glfwTerminate();
+        cerr << "Error initializing GL3W!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+    // Set vsync on
+    glfwSwapInterval(1);
+
+    // Init GL settings
+    glViewport(0, 0, XRES, YRES);
+    glClearColor(0.f, 0.f, 0.f, 1.f);
+
+    GLenum error = glGetError();
+    if(error != GL_NO_ERROR) {
+        glfwDestroyWindow(windowPtr);
+        glfwTerminate();
+        cerr << "Error initializing GL!" << endl;
+        exit(EXIT_FAILURE);
+    }
+
+#ifdef GUI
+    // Setup imgui
+    ImGui_ImplGlfwGL3_Init(windowPtr, true);
+    ImGuiWindowFlags logWindowFlags= 0;
+    logWindowFlags |= ImGuiWindowFlags_NoTitleBar;
+    logWindowFlags |= ImGuiWindowFlags_AlwaysAutoResize;
+    bool showLog = true;
+
+    Logger logger;
+    logger.AddLog("[gl] Context: %s\n     GLSL: %s\n",
+                   glGetString(GL_VERSION),
+                   glGetString(GL_SHADING_LANGUAGE_VERSION));
+
+
+    // Capture cout for logging
+    std::stringstream logCout;
+    std::streambuf* oldCout = std::cout.rdbuf(logCout.rdbuf());
+#endif // GUI
+
+    // Set glfw-callbacks, these will pass to imgui's callbacks if overridden
+    glfwSetWindowSizeCallback(windowPtr, windowSizeCallback);
+    glfwSetKeyCallback(windowPtr, keyCallback);
+    glfwSetCursorPosCallback(windowPtr, cursorCallback);
+    glfwSetMouseButtonCallback(windowPtr, mouseButtonCallback);
+
+    Quad q;
+
+    // Set up scene
+    std::string vertPath(RES_DIRECTORY);
+    vertPath += "shader/basic_vert.glsl";
+    std::string fragPath(RES_DIRECTORY);
+    fragPath += "shader/basic_frag.glsl";
+    ShaderProgram shader(vertPath, fragPath, "");
+
+    Timer rT;
+    Timer gT;
+    GpuProfiler sceneProf(5);
+
+    // Run the main loop
+    while (!glfwWindowShouldClose(windowPtr)) {
+        glfwPollEvents();
+
+#ifdef GUI
+        ImGui_ImplGlfwGL3_NewFrame();
+#endif // GUI
+
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+#ifdef GUI
+        // Update imgui
+        {
+            ImGui::SetNextWindowSize(ImVec2(LOGW, LOGH), ImGuiSetCond_Once);
+            ImGui::SetNextWindowPos(ImVec2(LOGM, YRES - LOGH - LOGM), ImGuiSetCond_Always);
+            ImGui::Begin("Log", &showLog, logWindowFlags);
+            ImGui::Text("Frame: %.1f Scene: %.1f", 1000.f / ImGui::GetIO().Framerate, sceneProf.getAvg());
+            if (logCout.str().length() != 0) {
+                logger.AddLog("%s", logCout.str().c_str());
+                logCout.str("");
+            }
+            logger.Draw();
+            ImGui::End();
+        }
+#endif // GUI
+
+        // Try reloading the shader every 0.5s
+        if (rT.getSeconds() > 0.5f) {
+            shader.reload();
+            rT.reset();
+        }
+
+        sceneProf.startSample();
+        shader.bind();
+        glUniform1f(shader.getULoc("uGT"), gT.getSeconds());
+        GLfloat res[] = {static_cast<GLfloat>(XRES), static_cast<GLfloat>(YRES)};
+        glUniform2fv(shader.getULoc("uRes"), 1, res);
+        glUniform2fv(shader.getULoc("uMPos"), 1, CURSOR_POS);
+        q.render();
+        sceneProf.endSample();
+
+#ifdef GUI
+        ImGui::Render();
+#endif // GUI
+
+        glfwSwapBuffers(windowPtr);
+    }
+
+#ifdef GUI
+    std::cout.rdbuf(oldCout);
+    ImGui_ImplGlfwGL3_Shutdown();
+#endif // GUI
+
+    glfwDestroyWindow(windowPtr);
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
+}
