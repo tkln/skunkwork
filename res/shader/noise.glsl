ad0a56e51c41d9f8627055bd29e62d57677f5ec3
Santeri Salmijärvi <santeri.salmijarvi@iki.fi>
2019-05-21 18:27:56 +0300

    Noise: Use simpler noise generation

--- res/shader/noise.glsl
+++ res/shader/noise.glsl
@@ -1,119 +1,36 @@
-//------------------------------- noise3D.glsl --------------------------------
-// Modified and cleaned up from the original
-
-// Description : Array and textureless GLSL 2D/3D/4D simplex 
-//               noise functions.
-//      Author : Ian McEwan, Ashima Arts.
-//  Maintainer : stegu
-//     Lastmod : 20110822 (ijm)
-//     License : MIT License
-//
-//Copyright (C) 2011 by Ashima Arts
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
-// https://github.com/ashima/webgl-noise
-// https://github.com/stegu/webgl-noise
-
-vec3 mod289(vec3 x) { return x - floor(x * 0.00346020761) * 289.0; }
-
-vec4 mod289(vec4 x) { return x - floor(x * 0.00346020761) * 289.0; }
-
-vec4 permute(vec4 x) { return mod289(((x * 34.0) + 1.0) * x); }
-
-vec4 taylorInvSqrt(vec4 r) { return 1.792842914 - 0.8537347209 * r; }
-
-float snoise(vec3 v)
+// 3D noise function with tweaks (IQ, Shane)
+// Range [0, 1]
+// https://www.shadertoy.com/view/lstGRB
+float noise(vec3 p)
 {
-    const vec2 C = vec2(0.166666667, 0.333333333);
-    const vec4 D = vec4(0.0, 0.5, 1.0, 2.0);
+    // Stride parameters
+    const vec3 s = vec3(7, 17, 13);
+    // Cell id
+    vec3 ip = floor(p);
+    // IQ's magic
+    vec4 h = vec4(0, s.yz, s.y + s.z) + dot(ip, s);
+    // Cell fraction
+    p -= ip;
+    // Cubic smoothing
+    p = p*p*(3 - 2 * p);
+    // Generate noise values for cube corners and mix along axes
+    h = mix(fract(sin(h) * 43758.5453), fract(sin(h + s.x) * 43758.5453), p.x);
+    h.xy = mix(h.xz, h.yw, p.y);
+    return mix(h.x, h.y, p.z);
+}
 
-    // First corner
-    vec3 i = floor(v + dot(v, C.yyy));
-    vec3 x0 = v - i + dot(i, C.xxx);
-
-    // Other corners
-    vec3 g = step(x0.yzx, x0.xyz);
-    vec3 l = 1.0 - g;
-    vec3 i1 = min(g.xyz, l.zxy);
-    vec3 i2 = max(g.xyz, l.zxy);
-
-    vec3 x1 = x0 - i1 + C.xxx;
-    vec3 x2 = x0 - i2 + C.yyy;
-    vec3 x3 = x0 - D.yyy;
-
-    // Permutations
-    i = mod289(i);
-    vec4 p = permute(permute(permute(i.z + vec4(0.0, i1.z, i2.z, 1.0)) +
-                             i.y + vec4(0.0, i1.y, i2.y, 1.0)) +
-                     i.x + vec4(0.0, i1.x, i2.x, 1.0));
-
-    // Gradients: 7x7 points over a square, mapped onto an octahedron.
-    // The ring size 17*17 = 289 is close to a multiple of 49 (49*6 = 294)
-    float n_ = 0.142857142; // 1.0/7.0
-    vec3  ns = n_ * D.wyz - D.xzx;
-
-    vec4 j = p - 49.0 * floor(p * ns.z * ns.z);
-
-    vec4 x_ = floor(j * ns.z);
-    vec4 y_ = floor(j - 7.0 * x_ );
-
-    vec4 x = x_ * ns.x + ns.yyyy;
-    vec4 y = y_ * ns.x + ns.yyyy;
-    vec4 h = 1.0 - abs(x) - abs(y);
-
-    vec4 b0 = vec4(x.xy, y.xy);
-    vec4 b1 = vec4(x.zw, y.zw);
-
-    vec4 s0 = floor(b0) * 2.0 + 1.0;
-    vec4 s1 = floor(b1) * 2.0 + 1.0;
-    vec4 sh = -step(h, vec4(0.0));
-
-    vec4 a0 = b0.xzyw + s0.xzyw * sh.xxyy;
-    vec4 a1 = b1.xzyw + s1.xzyw * sh.zzww;
-
-    vec3 p0 = vec3(a0.xy, h.x);
-    vec3 p1 = vec3(a0.zw, h.y);
-    vec3 p2 = vec3(a1.xy, h.z);
-    vec3 p3 = vec3(a1.zw, h.w);
-
-    //Normalise gradients
-    vec4 norm = taylorInvSqrt(vec4(dot(p0, p0), dot(p1, p1), dot(p2, p2), dot(p3, p3)));
-    p0 *= norm.x;
-    p1 *= norm.y;
-    p2 *= norm.z;
-    p3 *= norm.w;
-
-    // Mix final noise value
-    vec4 m = max(0.6 - vec4(dot(x0, x0), dot(x1, x1), dot(x2, x2), dot(x3, x3)), 0.0);
-    m = m * m;
-    return 42.0 * dot(m * m, vec4(dot(p0, x0), dot(p1, x1), dot(p2, x2), dot(p3, x3)));
-  }
-
-//--------------------------- end of noise3D.glsl -----------------------------
+// Noise with smooth animation
+float snoise(vec3 p)
+{
+    return mix(noise(p - 1.5), noise(p), 0.5);
+}
 
 // Simplified from Physically Based Rendering by Pharr et. al.
 float fbm(vec3 p, float omega, int octaves) {
     float sum = 0, lambda = 1, o = 1;
     for (int i = 0; i < octaves; ++i) {
         sum += o * snoise(lambda * p);
-        lambda *= 1.99f;
+        lambda *= 1.99;
         o *= omega;
     }
     return sum;
